f9922b94f83161ecd91de26d83e44fe05c2d0052
shwetal solanki <67383817+shwetal3@users.noreply.github.com>
2020-07-22 20:06:41 +0530

    Update dark.js

--- Day6/question3/dark.js
+++ Day6/question3/dark.js
@@ -1,7 +1,18 @@
 let name=prompt("Enter your name");
-const dark= document.getElementById("darks");
 const user=	document.getElementById("user");
 user.innerText="Hey "+ name + " Welcome to the page";
-dark.classList.add("body");
-dark.classList.toggle('body');
-dark.classList.toggle('dmode');
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("darkmode");
+}
+const times=document.getElementById("clocks");
+function clock()
+{
+	let date= new Date();
+	let time=date.toLocaleTimeString();
+	times.innerText=time;
+}
+myFunction();
+setInterval(clock,1000);
+
